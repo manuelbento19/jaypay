fe6d537273798b900efa6643ce1768110355b2ef
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-11-26 11:38:09 +0100

    feat: account model

---
+++ src/main/java/com/bentoo/jaypay/model/Account.java
@@ -0,0 +1,33 @@
+package com.bentoo.jaypay.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID  id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+    private String password;
+    private String phone;
+    private String verifyCode;
+
+    private LocalDateTime verifiedAt;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
