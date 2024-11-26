aa891ca32f0b7f42af40375954cd87e2cc68cba8
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-11-26 10:45:31 +0100

    feat: initialize

---
+++ src/main/java/com/bentoo/jaypay/JaypayApplication.java
@@ -0,0 +1,13 @@
+package com.bentoo.jaypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaypayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaypayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bentoo/jaypay/JaypayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bentoo.jaypay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaypayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
