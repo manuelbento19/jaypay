1f5e3623d3a0ac4adf888f946adf8ed87ea80f03
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-11-26 11:40:45 +0100

    feat: account repository

---
+++ src/main/java/com/bentoo/jaypay/repository/IAccountRepository.java
@@ -0,0 +1,10 @@
+package com.bentoo.jaypay.repository;
+
+import com.bentoo.jaypay.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface IAccountRepository  extends JpaRepository<Account, UUID> {
+
+}
